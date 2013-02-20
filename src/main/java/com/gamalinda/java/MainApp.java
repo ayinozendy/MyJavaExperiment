e64a010e849abab0ac87f0c7c3d827c7dc9078c1
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-02-21 00:02:32 +0800

    Restructed MainApp

--- src/main/java/com/gamalinda/java/MainApp.java
+++ src/main/java/com/gamalinda/java/MainApp.java
@@ -2,7 +2,21 @@ package com.gamalinda.java;
 
 public class MainApp {
 
+	private static MainApp mainAppInstace = new MainApp();
+
+	private MainApp() {
+
+	}
+	
 	public static void main(String[] args) {
+		getInstance().run();
+	}
+
+	public static MainApp getInstance() {
+		return mainAppInstace;
+	}
+
+	public void run() {
 		System.out.println("Hello World");
 	}
 }
