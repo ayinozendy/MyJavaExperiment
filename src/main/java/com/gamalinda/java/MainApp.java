06ce9a7d2e869246d611f2638fa963fc535f2de5
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-02-21 00:41:21 +0800

    Put args in list then print
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

--- src/main/java/com/gamalinda/java/MainApp.java
+++ src/main/java/com/gamalinda/java/MainApp.java
@@ -1,17 +1,21 @@
 package com.gamalinda.java;
 
+import java.util.*;
 import com.gamalinda.java.util.Log;
 
 public class MainApp {
 	private static final String TAG = MainApp.class.getSimpleName();
 
 	private static MainApp mainAppInstace = new MainApp();
+	private static boolean running = true;
 
-	private MainApp() {
+	private static List<String> argsList;
 
-	}
+	private MainApp() {}
 
 	public static void main(String[] args) {
+
+		argsList = Arrays.asList(args);
 		getInstance().run();
 	}
 
@@ -22,5 +26,9 @@ public class MainApp {
 	public void run() {
 		System.out.println("Hello World");
 		Log.d(TAG, "Test!");
+
+		for(String arg : argsList) {
+			System.out.println(arg);
+		}
 	}
 }
