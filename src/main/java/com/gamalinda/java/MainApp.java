ab9aa4ec312c561b1238d6879bd34af3a591c627
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-02-21 00:16:03 +0800

    Use custom Log
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

--- src/main/java/com/gamalinda/java/MainApp.java
+++ src/main/java/com/gamalinda/java/MainApp.java
@@ -1,13 +1,16 @@
 package com.gamalinda.java;
 
+import com.gamalinda.java.util.Log;
+
 public class MainApp {
+	private static final String TAG = MainApp.class.getSimpleName();
 
 	private static MainApp mainAppInstace = new MainApp();
 
 	private MainApp() {
 
 	}
-	
+
 	public static void main(String[] args) {
 		getInstance().run();
 	}
@@ -18,5 +21,6 @@ public class MainApp {
 
 	public void run() {
 		System.out.println("Hello World");
+		Log.d(TAG, "Test!");
 	}
 }
