74db98fc17587d1d568adfb7366890cf27348507
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-02-22 16:35:29 +0800

    Extracted Features Window MenuBar handling

--- src/main/java/com/gamalinda/java/jframe/FeaturesWindow.java
+++ src/main/java/com/gamalinda/java/jframe/FeaturesWindow.java
@@ -1,27 +1,14 @@
 package com.gamalinda.java.jframe;
 
-import com.gamalinda.java.util.Log;
+import com.gamalinda.java.jframe.jmenubar.FeaturesWindowMenuBar;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class FeaturesWindow implements ActionListener {
+public class FeaturesWindow {
     private static final String TAG = FeaturesWindow.class.getSimpleName();
 
     private static JFrame mainFrame;
-    private static JMenuBar menuBar;
-    private static JMenu fileMenu;
-    private static JMenu featuresMenu;
-
-    //Desktop screen dimensions
-    private static double SCREEN_WIDTH;
-    private static double SCREEN_HEIGHT;
-
-    //Menu Item Names/Action
-    private static final String EXIT = "Exit";
-    private static final String WRITE_TO_SCREEN = "Write to Screen";
+    private static FeaturesWindowMenuBar mainMenuBar;
 
     private FeaturesWindow() {
     }
@@ -36,30 +23,8 @@ public class FeaturesWindow implements ActionListener {
     }
 
     public void showMenuBar() {
-        menuBar = new JMenuBar(); //The Menu Bar on Windows
-        fileMenu = new JMenu("File"); //The Menu Bar Menu item
-        featuresMenu = new JMenu("Features");
-
-        menuBar.add(fileMenu); //Add the menus to the menu bar
-        showFileMenuItems();
-
-        menuBar.add(featuresMenu);
-        showFeaturesMenuItems();
-
-        mainFrame.setJMenuBar(menuBar);
-    }
-
-    private void showFileMenuItems() {
-        JMenuItem exitMenuItem = new JMenuItem(EXIT); //Menu item
-        exitMenuItem.addActionListener(this);
-        fileMenu.add(exitMenuItem); //Adding a menu item to a menu
-    }
-
-    private void showFeaturesMenuItems() {
-        JMenuItem writeToScreenItem = new JMenuItem(WRITE_TO_SCREEN);
-        writeToScreenItem.addActionListener(this);
-
-        featuresMenu.add(writeToScreenItem);
+        mainMenuBar = new FeaturesWindowMenuBar();
+        mainFrame.setJMenuBar(mainMenuBar.getMenuBar());
     }
 
     public void showWindow() {
@@ -68,39 +33,5 @@ public class FeaturesWindow implements ActionListener {
 
     public void run() {
 
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand().equals(EXIT)) {
-            Log.d(TAG, "exitApp()"); //Should log first before exit app because it won't be reached when exit
-            exitApp();
-        } else if (e.getActionCommand().equals(WRITE_TO_SCREEN)) {
-            writeOnScreen();
-            Log.d(TAG, "writeOnScreen()");
-        }
-    }
-
-    private void exitApp() {
-        System.exit(0);
-    }
-
-    private void writeOnScreen() {
-        JWindow w = new JWindow(); //A frameless window
-        w.add(new JLabel("Hello World!"));
-
-        getScreenDimensions();
-        int centerHorizontal = (int) (SCREEN_WIDTH / 2);
-        int centerVertical = (int) (SCREEN_HEIGHT / 2);
-
-        w.setLocation(centerHorizontal, centerVertical);
-        w.pack(); //Causes this Window to be sized to fit the preferred size and layouts of its subcomponents.
-        w.setVisible(true);
-    }
-
-    private void getScreenDimensions() {
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        SCREEN_WIDTH = screenSize.getWidth();
-        SCREEN_HEIGHT = screenSize.getHeight();
     }
 }
