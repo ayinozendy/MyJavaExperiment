a39cd6c58fd07ac48dc1735649731eac8538a273
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-03-02 15:51:11 +0800

    Switched JWindow to JFrame
    Because JWindow doesn't seem to respond on KeyListeners, it might be a bug or a feature.
    Close the frame when Escape button is released.
    
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

--- src/main/java/com/gamalinda/java/jwindow/WriteToScreenFeature.java
+++ src/main/java/com/gamalinda/java/jwindow/WriteToScreenFeature.java
@@ -8,21 +8,27 @@ import java.awt.event.KeyListener;
 public class WriteToScreenFeature implements KeyListener {
     private static final String TAG = WriteToScreenFeature.class.getSimpleName();
 
+    private static final int ESCAPE_KEY_CODE = 27;
+
     //Desktop screen dimensions
     private static double SCREEN_WIDTH;
     private static double SCREEN_HEIGHT;
 
+    JFrame frame;
+
     public void execute() {
-        JWindow w = new JWindow(); //A frameless window
-        w.add(new JLabel("Hello World!"));
+        frame = new JFrame(); //A frameless window
+        frame.add(new JLabel("Hello World!"));
+        frame.setUndecorated(true);
+        frame.addKeyListener(this);
 
         getScreenDimensions();
         int centerHorizontal = (int) (SCREEN_WIDTH / 2);
         int centerVertical = (int) (SCREEN_HEIGHT / 2);
 
-        w.setLocation(centerHorizontal, centerVertical);
-        w.pack(); //Causes this Window to be sized to fit the preferred size and layouts of its subcomponents.
-        w.setVisible(true);
+        frame.setLocation(centerHorizontal, centerVertical);
+        frame.pack(); //Causes this Window to be sized to fit the preferred size and layouts of its subcomponents.
+        frame.setVisible(true);
     }
 
     private void getScreenDimensions() {
@@ -43,5 +49,8 @@ public class WriteToScreenFeature implements KeyListener {
 
     @Override
     public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == ESCAPE_KEY_CODE) {
+            frame.dispose();
+        }
     }
 }
