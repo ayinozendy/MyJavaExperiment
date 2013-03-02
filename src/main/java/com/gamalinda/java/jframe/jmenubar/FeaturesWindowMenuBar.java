1d460bd7912b24cf53a7cff3a06c25f671879216
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-03-02 21:36:04 +0800

    Refactor execution of features
    Removed unecessary methods.

--- src/main/java/com/gamalinda/java/jframe/jmenubar/FeaturesWindowMenuBar.java
+++ src/main/java/com/gamalinda/java/jframe/jmenubar/FeaturesWindowMenuBar.java
@@ -6,7 +6,6 @@ import com.gamalinda.java.jframe.WriteToScreenFeature;
 import com.gamalinda.java.util.Log;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
